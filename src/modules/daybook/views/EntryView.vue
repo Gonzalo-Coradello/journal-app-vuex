3c2d4f92d028ae61945e318d9960210044ba0843
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-12-29 16:49:05 -0300

    create EntryView component

---
+++ src/modules/daybook/views/EntryView.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="entry-title d-flex justify-content-between p-2">
+    <div>
+      <span class="text-success fs-3 fw-bold">29</span>
+      <span class="mx-2 fs-3">diciembre</span>
+      <span class="mx-2 fs-4 fw-light">2023, viernes</span>
+    </div>
+
+    <div>
+      <button class="btn btn-danger mx-2">
+        Borrar
+        <i class="fa fa-trash-alt"></i>
+      </button>
+      <button class="btn btn-danger mx-2">
+        Subit foto
+        <i class="fa fa-upload"></i>
+      </button>
+    </div>
+  </div>
+
+  <hr />
+  <div class="d-flex flex-column px-3 h-75">
+    <textarea placeholder="¿Qué sucedió hoy?"></textarea>
+  </div>
+
+  <Fab icon="fa-save" />
+
+  <img
+    src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Beautiful_landscape.JPG/1200px-Beautiful_landscape.JPG?20130221170937"
+    alt="entry-picture"
+    class="img-thumbnail"
+  />
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+
+export default {
+  components: {
+    Fab: defineAsyncComponent(() => import('../components/Fab.vue'))
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+textarea {
+  font-size: 20px;
+  border: none;
+  height: 100vh;
+
+  &:focus {
+    outline: none;
+  }
+}
+
+img {
+  width: 200px;
+  position: fixed;
+  bottom: 150px;
+  right: 20px;
+  box-shadow: 0px 5px 10px rgba($color: #000000, $alpha: 0.2);
+}
+</style>

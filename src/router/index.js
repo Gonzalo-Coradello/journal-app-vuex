993f9cd23f30ff1146bd0994331e6e261e337251
Gonzalo Coradello <gonzalocoradello@gmail.com>
2024-01-10 11:58:15 -0300

    add auth layout, views and router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import daybookRouter from '../modules/daybook/router'
+import authRouter from '../modules/auth/router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,12 +14,12 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    }
-    ,
+    },
+    {
+      path: '/auth',
+      ...authRouter
+    },
     {
       path: '/daybook',
       ...daybookRouter

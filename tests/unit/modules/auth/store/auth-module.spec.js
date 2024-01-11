24ad337fbfd9c5da0b10a652749fb1e23302fbdf
Gonzalo Coradello <gonzalocoradello@gmail.com>
2024-01-11 16:50:42 -0300

    test auth module getters

--- tests/unit/modules/auth/store/auth-module.spec.js
+++ tests/unit/modules/auth/store/auth-module.spec.js
@@ -64,4 +64,16 @@ describe('Vuex: Pruebas en el auth-module', () => {
     expect(localStorage.getItem('idToken')).toBe(null)
     expect(localStorage.getItem('refreshToken')).toBe(null)
   })
+
+  test('Getters: username - currentState', () => {
+    const store = createVuexStore({
+      status: 'authenticated',
+      user: { name: 'Gonzalo', email: 'gonzalo@gmail.com' },
+      idToken: 'ABC-123',
+      refreshToken: 'XYZ-123'
+    })
+
+    expect(store.getters['auth/currentState']).toBe('authenticated')
+    expect(store.getters['auth/username']).toBe('Gonzalo')
+  })
 })

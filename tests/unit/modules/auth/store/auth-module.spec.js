21c8c8dad3201b1ba576b4e08c9a65cbc047c52e
Gonzalo Coradello <gonzalocoradello@gmail.com>
2024-01-11 17:49:23 -0300

    test auth module actions

--- tests/unit/modules/auth/store/auth-module.spec.js
+++ tests/unit/modules/auth/store/auth-module.spec.js
@@ -1,5 +1,6 @@
 import { describe, expect, test } from 'vitest'
 import createVuexStore from '../../../mock-data/mock-store'
+import axios from 'axios'
 
 describe('Vuex: Pruebas en el auth-module', () => {
   test('estado inicial', () => {
@@ -75,5 +76,105 @@ describe('Vuex: Pruebas en el auth-module', () => {
 
     expect(store.getters['auth/currentState']).toBe('authenticated')
     expect(store.getters['auth/username']).toBe('Gonzalo')
+  })
+
+  test('Actions: createUser - Error: usuario ya existe', async () => {
+    const store = createVuexStore({
+      status: 'not-authenticated',
+      user: null,
+      idToken: null,
+      refreshToken: null
+    })
+
+    const newUser = { name: 'Test User', email: 'test@test.com', password: '123456' }
+
+    const resp = await store.dispatch('auth/createUser', newUser)
+
+    expect(resp).toEqual({ ok: false, message: 'EMAIL_EXISTS' })
+
+    const { status, user, idToken, refreshToken } = store.state.auth
+
+    expect(status).toBe('not-authenticated')
+    expect(user).toBe(null)
+    expect(idToken).toBe(null)
+    expect(refreshToken).toBe(null)
+  })
+
+  test('Actions: createUser, signInUser - Crea el usuario', async () => {
+    const store = createVuexStore({
+      status: 'not-authenticated',
+      user: null,
+      idToken: null,
+      refreshToken: null
+    })
+
+    const newUser = { name: 'Test User 2', email: 'test2@test.com', password: '123456' }
+
+    await store.dispatch('auth/signInUser', { ...newUser })
+    const { idToken } = store.state.auth
+
+    const API_KEY = import.meta.env.VITE_FIREBASE_API_KEY
+
+    await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:delete?key=${API_KEY}`, {
+      idToken
+    })
+
+    const resp = await store.dispatch('auth/createUser', newUser)
+
+    expect(resp).toEqual({ ok: true })
+
+    const { status, user, idToken: token, refreshToken } = store.state.auth
+
+    expect(status).toBe('authenticated')
+    expect(user).toMatchObject({ name: 'Test User 2', email: 'test2@test.com' })
+    expect(typeof token).toBe('string')
+    expect(typeof refreshToken).toBe('string')
+  })
+
+  test('Actions: checkAuthentication - POSITIVA', async () => {
+    const store = createVuexStore({
+      status: 'not-authenticated',
+      user: null,
+      idToken: null,
+      refreshToken: null
+    })
+
+    await store.dispatch('auth/signInUser', { email: 'test@test.com', password: '123456' })
+    const { idToken } = store.state.auth
+    store.commit('auth/logout')
+
+    localStorage.setItem('idToken', idToken)
+
+    const checkResp = await store.dispatch('auth/checkAuthentication')
+
+    const { status, user, idToken: token } = store.state.auth
+
+    expect(checkResp).toEqual({ ok: true })
+
+    expect(status).toBe('authenticated')
+    expect(user).toMatchObject({ name: 'User Test', email: 'test@test.com' })
+    expect(typeof token).toBe('string')
+  })
+
+  test('Actions: checkAuthentication - NEGATIVA', async () => {
+    const store = createVuexStore({
+      status: 'authenticating',
+      user: null,
+      idToken: null,
+      refreshToken: null
+    })
+
+    localStorage.removeItem('idToken')
+    const checkResp1 = await store.dispatch('auth/checkAuthentication')
+
+    expect(checkResp1).toEqual({ ok: false, message: 'No hay token' })
+    expect(store.state.auth.user).toBeFalsy()
+    expect(store.state.auth.idToken).toBeFalsy()
+    expect(store.state.auth.status).toBe('not-authenticated')
+
+    localStorage.setItem('idToken', 'ABC-123')
+    const checkResp2 = await store.dispatch('auth/checkAuthentication')
+    expect(checkResp2).toEqual({ ok: false, message: 'INVALID_ID_TOKEN' })
+    expect(store.state.auth.status).toBe('not-authenticated')
   })
 })
